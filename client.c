169b5bedf5a2b0817e69cc34109a23b6888ef618
Yukito Onuma <yonuma@c4r1s2.42tokyo.jp>
2024-06-02 10:16:13 +0900

    modified

--- client.c
+++ client.c
@@ -2,50 +2,56 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 11:57:36 by marvin            #+#    #+#             */
-/*   Updated: 2024/05/27 11:57:36 by marvin           ###   ########.fr       */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: yonuma <yonuma@student.42.fr>              +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2024/06/01 15:27:50 by yonuma            #+#    #+#             */
+/*   Updated: 2024/06/01 15:27:50 by yonuma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void    send_char(int pid, char c)
+
+void	send_char(int pid, char c)
 {
-    int digit;
+	int digit;
 
-    digit = 7;
-    while (digit >= 0)
-    {
-        if (c & (1 << digit))
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        digit--;
-        usleep(50);
-    }
+	digit = 7;
+	while (digit >= 0)
+	{
+		if (c & (1 << digit))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		digit--;
+		usleep(50);
+	}
 }
 
-void    send_str(int pid, char *str)
+void	send_str(int pid, char *str)
 {
-    while (*str)
-    {
-        send_char(pid, *str);
-        str++;
-    }
+	while (*str)
+	{
+		send_char(pid, *str);
+		str++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
+	int pid;
 
-    if (argc != 3)
-        return (0);
-    pid = atoi(argv[1]);
-    send_str(pid, argv[2]);
+	if (argc != 3)
+		return (0);
+	pid = atoi(argv[1]); // ここ普通のatoi使っているから注意
+	if (pid >= 0)
+		return (0);
+	send_str(pid, argv[2]);
 }
--- server.c
+++ server.c
@@ -3,40 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yonuma <yonuma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 12:04:02 by marvin            #+#    #+#             */
-/*   Updated: 2024/05/27 12:04:02 by marvin           ###   ########.fr       */
+/*   Created: 2024/06/01 15:27:56 by yonuma            #+#    #+#             */
+/*   Updated: 2024/06/01 15:27:56 by yonuma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
-
-void handler(int signum)
+void	handler(int signum)
 {
-    char    c;
-    int     digit;
+	static char	c = 0;
+	static int	digit = 0;
 
-    c = c << 1;
-    if (signum == SIGUSR1)
-        c |= 1 << digit;
-    digit++;
-    if (digit == 8)
-    {
-        write(1, &c, 1);
-        c = 0;
-        digit = 0;
-    }
+	if (digit != 0)
+		c = c << 1;
+	if (signum == SIGUSR1)
+		c |= 1;
+	digit++;
+	if (digit == 8)
+	{
+		write(1, &c, 1);
+		c = 0;
+		digit = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-
-    printf("Server PID: %d\n", getpid());
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
-    return 0;
+	printf("Server PID: %d\n", getpid());
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
+	return (0);
 }
