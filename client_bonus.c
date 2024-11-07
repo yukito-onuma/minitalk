b6312f926081d879ea093ca82c41fa0ff770d62b
Yukito Onuma <yonuma@c5r3s5.42tokyo.jp>
2024-11-07 21:12:57 +0900

    たくさん文句を言ったminitalkも遂に終わりを迎えました。最終的にはusleepすらいらず、４２で最速です。ありがとうterminal

--- client_bonus.c
+++ client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yonuma <yonuma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 18:26:49 by yonuma            #+#    #+#             */
-/*   Updated: 2024/11/04 20:33:17 by yonuma           ###   ########.fr       */
+/*   Updated: 2024/11/07 21:08:12 by yonuma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	send_char(int pid, char c)
 		{
 			printf("%d\n", skip_char);
 		}
-		usleep(500);
+		usleep(100);
 	}
 }
 
